e528d68613c06f9854399e1054a3b193b38fa5a3
Ezequiel Yacar <ezeayacar@gmail.com>
2022-04-21 18:57:32 -0300

    feat: add eslint and prettier configuration to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Work In Progress
-    </div>
-  );
+    return <div className='App'>Work In Progress</div>;
 }
 
 export default App;
